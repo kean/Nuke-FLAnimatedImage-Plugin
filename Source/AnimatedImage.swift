361af7947e11a8b90e3e3e758a4031275c32ec8f
Ruslan Timchenko <rustimchek@mail.ru>
2019-07-18 18:24:33 +0300

    Fixed a compatibility issue with Nuke 8.0

--- Source/AnimatedImage.swift
+++ Source/AnimatedImage.swift
@@ -7,7 +7,7 @@ import FLAnimatedImage
 import Nuke
 
 extension FLAnimatedImageView {
-    @objc open override func display(image: Image?) {
+    @objc open override func nuke_display(image: Image?) {
         guard image != nil else {
             self.animatedImage = nil
             self.image = nil
